abf2ffca4e74faac35089209ef933c0858c3997e
Ana Maria Souza <anamariasouza701@gmail.com>
2025-01-13 21:53:25 -0400

    subindo projeto para o git hub

---
+++ codigo.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import pyautogui
+import time
+
+pyautogui.PAUSE = 1.5
+
+# abrir o sistema
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+time.sleep(2)
+pyautogui.click(x=332, y=679)
+time.sleep(2)
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(2)
+
+# logar no sistema
+pyautogui.press("tab")
+pyautogui.write("email@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("123")
+pyautogui.press("enter")
+
+# importar a base de dados dos produtos
+tabela = pd.read_csv("produtos.csv")
+print(tabela)
+time.sleep(1.5)
+
+# cadastrar 1 produto
+for linha in tabela.index:
+  pyautogui.click(x=757, y=258)
+  codigo = tabela.loc[linha, "codigo"]
+  pyautogui.write(str(codigo))
+  pyautogui.press("tab")
+
+  pyautogui.write(str(tabela.loc[linha, "marca"]))
+  pyautogui.press("tab")
+  pyautogui.write(str(tabela.loc[linha, "tipo"]))
+  pyautogui.press("tab")
+  pyautogui.write(str(tabela.loc[linha, "categoria"]))
+  pyautogui.press("tab")
+  pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+  pyautogui.press("tab")
+  pyautogui.write(str(tabela.loc[linha, "custo"]))
+  pyautogui.press("tab")
+  obs = tabela.loc[linha, "obs"]
+  if not pd.isna(obs):
+     pyautogui.write(str(tabela.loc[linha, "obs"]))
+  pyautogui.press("enter")
+
+  #subir com scroll
+  pyautogui.scroll(5000)
+  time.sleep(.5)
